0bc3e82603993c01f98b9bb3153457cc2b6b80eb
tallywiesenberg <t.j.wies@gmail.com>
2022-06-20 21:07:55 -0400

    depl script

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,4 +1,6 @@
 require("@nomiclabs/hardhat-waffle");
+require("@nomiclabs/hardhat-etherscan");
+require('dotenv').config()
 
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
@@ -18,4 +20,15 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
  */
 module.exports = {
   solidity: "0.8.4",
+  networks: {
+    hardhat: {
+    },
+    rinkeby: {
+      url: process.env.NODE_URL,
+      accounts: [privateKey1, privateKey2,]
+    }
+  },
+  etherscan: {
+    apiKey: process.env.ETHERSCAN_KEY
+  }
 };
